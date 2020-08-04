c1a9c29e1731045ae325eec8614c1e853991c9cf
udayanshevade <udayan.shevade@gmail.com>
2020-08-03 20:05:07 -0700

    add create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to new_contact_path, notice: 'Message sent successfully' }
+      else
+        format.html { redirect_to new_contact_path, notice: 'Unable to send message' }
+      end
+    end
   end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
