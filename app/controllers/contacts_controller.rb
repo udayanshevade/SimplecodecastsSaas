e369b42a6e53d766544943b0e8622a257da8c851
udayanshevade <udayan.shevade@gmail.com>
2020-08-05 01:57:17 -0700

    Add ContactMailer, call contact_email method when contact is saved

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,11 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
 
     if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      comments = params[:contact][:comments]
+      ContactMailer.contact_email(name, email, comments).deliver
+
       flash[:notice] = 'Message sent successfully';
       redirect_to new_contact_path
     else
