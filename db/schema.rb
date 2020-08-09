2780ff8b2952e95f768b08d34e2725e2f2171aaf
udayanshevade <udayan.shevade@gmail.com>
2020-08-08 17:00:38 -0700

    migration for relation between users and plans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_234424) do
+ActiveRecord::Schema.define(version: 2020_08_08_235630) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_234424) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "plan_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
